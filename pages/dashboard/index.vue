39191fe7b2ad5aec0feec22536614496b37a4804
Jay Solanki <solankijay062@gmail.com>
2023-12-20 17:59:48 +0530

    - Project InIt

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Dashboard</h1>
+    <br/>
+    <h4>
+        Go To
+        <NuxtLink to="/">Home</NuxtLink>
+    </h4>
+</template>
+<script setup></script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+      <div>
+    <h1>Hello,{{ userName }}</h1>
+  </div>
+  <br/>
+  <h4>Go to 
+      <NuxtLink to="/dashboard">dashboard</NuxtLink>
+  </h4>
+</template>
+<script setup>
+const userName = 'Chiku'
+</script>
